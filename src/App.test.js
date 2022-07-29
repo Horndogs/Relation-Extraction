57f20cf3a8d4e0c30948744bc5fa1e501d50f8d4
Horndogs <100391811+Horndogs@users.noreply.github.com>
2022-07-29 08:34:43 +0000

    cns

--- src/App.test.js
+++ src/App.test.js
@@ -2,3 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { render } from 'react-testing-library';
+
+it('renders without crashing', () => {
+  const div = 
