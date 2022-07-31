298a6f7140603fff05a39fbd89265644518553d3
Horndogs <100391811+Horndogs@users.noreply.github.com>
2022-07-31 14:17:47 +0000

    update cfg

--- src/App.test.js
+++ src/App.test.js
@@ -5,4 +5,9 @@ import { render } from 'react-testing-library';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div)
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+
+it('renders welc
