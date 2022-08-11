c862161d135197ad0c2b07b3ae1bdd73263cb434
Horndogs <100391811+Horndogs@users.noreply.github.com>
2022-08-11 20:42:13 +0000

    Updated about.html

--- src/projects.js
+++ src/projects.js
@@ -2,4 +2,5 @@ import React, { Component } from 'react'
 import ProjectDisplay from './project-display';
 import Icons from './language-icons'
 import projects from './project-data'
-import html f
+import html from './images/logos/html.png'
+import css from './imag
