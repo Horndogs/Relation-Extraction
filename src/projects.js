58dc609e6a98042695435570551fa14a9aec5128
Horndogs <100391811+Horndogs@users.noreply.github.com>
2022-08-23 18:05:52 +0000

    merge to trunk

--- src/projects.js
+++ src/projects.js
@@ -20,4 +20,4 @@ class Projects extends Component {
     return(
       <div className='projects'>
         <ProjectDisplay projectData={projects.codeMap} icons={[neo4j, node, express, react, redux, html, css]}/>
-        <ProjectDisplay projectData={pr
+        <ProjectDisplay projectData={projects.stockTicker} icons={[react, html, css, coinba
