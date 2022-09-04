dd119f3a3d8088d80091ba7d86c821f6cbb004e5
Horndogs <100391811+Horndogs@users.noreply.github.com>
2022-09-04 22:34:20 +0000

    Update the readme

--- src/projects.js
+++ src/projects.js
@@ -25,4 +25,4 @@ class Projects extends Component {
         <div className='projects displayBox'>
           <p className='projectHeader'>Words in Motion</p>
           <a className='visit' href='https://medium.com/@mewarrick/things-to-consider-when-using-css-animations-especially-with-react-b461f14e843f'>Read my Medium article on my experience</a>
-          <iframe width="350" height="197" src="https://www.
+          <iframe width="350" height="197" src="https://www.youtube.com/embed/4fVmH_u-8ic" frameBorder=
