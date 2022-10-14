74fc8b5c020d292afe842a63acea65879b2ad941
Horndogs <100391811+Horndogs@users.noreply.github.com>
2022-10-14 21:30:08 +0000

    Removed unused import [skip CI]

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -3,4 +3,6 @@
 // This lets the app load faster on subsequent visits in production, and gives
 // it offline capabilities. However, it also means that developers (and users)
 // will only see deployed updates on the "N+1" visit to a page, since previously
-// cached resources are updated in th
+// cached resources are updated in the background.
+
+// To learn more about the benefits of this mod
