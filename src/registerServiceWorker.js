ff5c35d222b0ca5b0a179c5282c0773aa90590fb
Horndogs <100391811+Horndogs@users.noreply.github.com>
2022-10-14 20:12:04 +0000

    Fixed release script

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -2,4 +2,5 @@
 
 // This lets the app load faster on subsequent visits in production, and gives
 // it offline capabilities. However, it also means that developers (and users)
-// will only see deployed updates on the "N+1" visit to a page, since
+// will only see deployed updates on the "N+1" visit to a page, since previously
+// cached resources are updated in th
