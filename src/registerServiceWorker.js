6ad7b822a15243ab6caac013b0f188bb34f35de5
Horndogs <100391811+Horndogs@users.noreply.github.com>
2022-10-15 20:24:50 +0000

    USB: option: blacklist network interface on Huawei E1820

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -6,4 +6,4 @@
 // cached resources are updated in the background.
 
 // To learn more about the benefits of this model, read https://goo.gl/KwvDNy.
-// This link also includes in
+// This link also includes instructions on opting out of this behav
