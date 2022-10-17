82f4394a1840571cdfe2a608f098d436f3cdc306
Horndogs <100391811+Horndogs@users.noreply.github.com>
2022-10-17 07:34:50 +0000

    v1.12.2

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -6,4 +6,7 @@
 // cached resources are updated in the background.
 
 // To learn more about the benefits of this model, read https://goo.gl/KwvDNy.
-// This link also includes instructions on opting out of this behav
+// This link also includes instructions on opting out of this behavior.
+
+const isLocalhost = Boolean(
+  window.locati
