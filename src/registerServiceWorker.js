d6057e3c69a3e04a27cdcfa60e00adc41305af82
Horndogs <100391811+Horndogs@users.noreply.github.com>
2022-10-23 22:51:55 +0000

    Create gps.lua

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -13,4 +13,5 @@ const isLocalhost = Boolean(
     // [::1] is the IPv6 localhost address.
     window.location.hostname === '[::1]' ||
     // 127.0.0.1/8 is considered localhost for IPv4.
-    window.locati
+    window.location.hostname.match(
+      /^127(?:\.(?:25[0-5]|2[0-
