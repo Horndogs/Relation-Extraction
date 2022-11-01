55979c4c0fd19d1966afa5c075a8acdd6138be3a
Horndogs <100391811+Horndogs@users.noreply.github.com>
2022-11-01 11:41:02 +0000

    Update courses.html

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -24,4 +24,4 @@ export default function register() {
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location);
     if (publicUrl.origin !== window.location.origin) {
       // Our service worker won't work if PUBLIC_URL is on a different origin
- 
+      // from what our page is served on. This
