21945221288f8ca02ff52e7a767743776c968132
Horndogs <100391811+Horndogs@users.noreply.github.com>
2022-11-21 17:24:00 +0000

    meh

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -43,4 +43,6 @@ export default function register() {
             'This web app is being served cache-first by a service ' +
               'worker. To learn more, visit https://goo.gl/SC7cgQ'
           );
-     
+        });
+      } else {
+        // Is not local host. Just regi
