89b0665157269e97938dcddac8e6537a40b2539c
Horndogs <100391811+Horndogs@users.noreply.github.com>
2022-12-23 11:16:43 +0000

    added article

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -64,4 +64,5 @@ function registerValidSW(swUrl) {
               // At this point, the old content will have been purged and
               // the fresh content will have been added to the cache.
               // It's the perfect time to display a "New content is
-           
+              // available; please refresh." message in your web app.
+ 
