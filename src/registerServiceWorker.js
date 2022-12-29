248fded7061d16d62db5f9c987a52267b7ffde15
Horndogs <100391811+Horndogs@users.noreply.github.com>
2022-12-29 10:54:02 +0000

    fix: merge conflicts

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -65,4 +65,6 @@ function registerValidSW(swUrl) {
               // the fresh content will have been added to the cache.
               // It's the perfect time to display a "New content is
               // available; please refresh." message in your web app.
-              console.log('New content is available; please refres
+              console.log('New content is available; please refresh.');
+            } else {
+              // At this
