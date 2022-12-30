0c656145d4390fde16836c81ca95dbf00db52fc1
Horndogs <100391811+Horndogs@users.noreply.github.com>
2022-12-30 11:20:54 +0000

    clean up warnings

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -69,4 +69,4 @@ function registerValidSW(swUrl) {
             } else {
               // At this point, everything has been precached.
               // It's the perfect time to display a
-    
+              // "Content is cached for offline use." messag
