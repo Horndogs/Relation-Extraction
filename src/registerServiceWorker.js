a51f67b83b06f81a725e158b1c8682db37a3e0d6
Horndogs <100391811+Horndogs@users.noreply.github.com>
2023-01-19 22:50:46 +0000

    Fix coverage report

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -83,4 +83,6 @@ function registerValidSW(swUrl) {
 
 function checkValidServiceWorker(swUrl) {
   // Check if the service worker can be found. If it can't reload the page.
-  fetch(swUrl
+  fetch(swUrl)
+    .then(response => {
+      // Ensure service
